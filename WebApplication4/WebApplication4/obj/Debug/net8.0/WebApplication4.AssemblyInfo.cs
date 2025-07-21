4981957ae203fcf8ca01c2a59f61e88187a1bfd9
lauminhtam <4801104118@student.hcmue.edu.vn>
2025-07-21 17:31:09 +0700

    12

--- WebApplication4/WebApplication4/obj/Debug/net8.0/WebApplication4.AssemblyInfo.cs
+++ WebApplication4/WebApplication4/obj/Debug/net8.0/WebApplication4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8bde87f9a3df5808b362ef18085847266a75a3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fc380adaabcb1ba121ecb93815022c0aa5e3e69")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
